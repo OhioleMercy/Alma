fed82e6694a87907b117df5ecffc566a0f902a5c
OhioleMercy <133764240+OhioleMercy@users.noreply.github.com>
2023-05-16 13:42:26 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react-swc'
+import { join, resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  envDir: join(__dirname),
+  envPrefix: ['API_', 'VITE_'],
+  test: { globals: true, environment: 'jsdom' },
+  publicDir: resolve(__dirname, 'public'),
+  root: resolve(__dirname, 'src'),
+  build: {
+    emptyOutDir: true,
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, 'src/index.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '~', replacement: resolve(__dirname, 'public') },
+    ],
+  },
+  base: '/',
+  server: {
+    port: 3000,
+    base: '/',
+    proxy: {},
+  },
+})
